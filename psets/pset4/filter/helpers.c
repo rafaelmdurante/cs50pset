a3537417a921a8bab4edaa033857e533fbb1d0f4
Rafael Miranda Durante <rafaelmdurante@gmail.com>
2020-06-27 14:07:18 +0100

    Add filter reflect to pset4

--- psets/pset4/filter/helpers.c
+++ psets/pset4/filter/helpers.c
@@ -8,10 +8,11 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int column = 0; column < width; column++)
         {
-            int average = round((image[row][column].rgbtRed +
-                                 image[row][column].rgbtGreen +
-                                 image[row][column].rgbtBlue) / 3);
-
+            // stores pixel to a variable called px
+            RGBTRIPLE px = image[row][column];
+            // calculates the average
+            int average = round((px.rgbtRed + px.rgbtGreen + px.rgbtBlue) / 3.0);
+            // change the original value to average
             image[row][column].rgbtRed = average;
             image[row][column].rgbtGreen = average;
             image[row][column].rgbtBlue = average;
@@ -22,7 +23,19 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    // for each row
+    for (int row = 0; row < height; row++)
+    {
+        // iterate over half of the image
+        for (int column = 0; column < (width / 2); column++)
+        {
+            // store a backup of the original pixel
+            RGBTRIPLE bkpPixel = image[row][column];
+            // swap pixels
+            image[row][column] = image[row][width - column - 1];
+            image[row][width - column - 1] = bkpPixel;
+        }
+    }
 }
 
 // Blur image
